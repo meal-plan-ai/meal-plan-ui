225cc6384e57dcfa62bf9110771b238a085ead3b
Yusypenko Alexander <o.yusipenko@gmail.com>
2025-05-12 19:06:25 +0200

    Fix production HTTPS and mixed content errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['www.meal-plan.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.meal-plan.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
